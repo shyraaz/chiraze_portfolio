6bca27ba00a378eed97d1fc43d76c02c20dfffc4
shyraaz <67177864+shyraaz@users.noreply.github.com>
2025-08-12 11:27:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -186,8 +186,30 @@ st.markdown("## contact")
 st.write("contact section")
 
 # Your page content
+import streamlit as st
+from transformers import pipeline
 
+st.title("Text To Audio App")
+# إعداد الـ pipeline
+pipe = pipeline("text-to-speech", model="suno/bark-small", device="cpu")
 
+# واجهة Streamlit
+col = st.columns(2)
+
+with col[0]: 
+    input_text = st.text_area("Enter Your Text")
+    btn = st.button("Text To Audio")
+    if btn and input_text.strip() != "":
+        with st.spinner("قاعدين نحضرو في الصوت..."):
+            output = pipe(input_text)
+            audio_bytes = output["audio"]
+            sampling_rate = output["sampling_rate"]
+            st.audio(audio_bytes, format="audio/wav", sample_rate=sampling_rate)
+    elif btn:
+        st.warning("رجاءً أدخل نص!")
+
+with col[1]:
+    st.image("images/text_to_audio.jpg") 
 
 
 
